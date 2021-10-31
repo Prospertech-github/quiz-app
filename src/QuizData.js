fff782061d0f062fd3635af746bbb1671b2911b0
Prosper Ikechukwu <mrhendrix62@gmail.com>
2021-10-31 23:01:27 +0100

    Initial commit

---
+++ src/QuizData.js
@@ -0,0 +1,45 @@
+export const QuizData = [
+    {
+        id: 0,
+        question: `Who is the organizer of the Learnable Program?`,
+        options: [`Decagon`, `Genesys`, `DigitaNotion`, `Teachable`],
+        answer: `Genesys`
+    },
+    {
+        id: 1,
+        question: `How many learning tracks exist in the Learnable program?`,
+        options: [`3`, `2`, `1`, `NOTA`],
+        answer: `3`
+    },
+    {
+        id: 2,
+        question: `Where is Genesys located?`,
+        options: [`Agulu`, `Awka`, `Abuja`, `Enugu`],
+        answer: `Enugu`
+    },
+    {
+        id: 3,
+        question: `What is the learning tool used for the Learnable program?`,
+        options: [`LearnIt`, `Teachable`, `Coursera`, `Slack`],
+        answer: `Teachable`
+    },
+    {
+        id: 4,
+        question: `Who is Jerry Uke?`,
+        options: [`An Intern`, `Coordinator`, `Janitor`, `NOTA`],
+        answer: `Coordinator`
+    },
+    {
+        id: 5,
+        question: `How many weeks is assigned for learning?`,
+        options: [`8 weeks`, `10 weeks`, `12 weeks`, `As Long As Possible`],
+        answer: `12 weeks`
+    },
+    {
+        id: 6,
+        question: `What programming language is frequently used in Genesys?`,
+        options: [`Kotlin`, `Variety`, `Java`, `JavaScript`],
+        answer: `JavaScript`
+    },
+    
+]
